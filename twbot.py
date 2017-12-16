cb5b2686432ab0109e3a4948d2a35bd30f221e2f
= <=>
2017-12-16 20:42:52 -0300

    Add support for tweets with shortened links
    More than one shortened link can be present on a tweet and each one will be commented following reddit link formatting style

--- twbot.py
+++ twbot.py
@@ -1,8 +1,10 @@
 import praw
-import re
 import tweepy
+import re
 import sqlite3
 import config
+import time
+import requests
 
 SUBREDDIT = "argentina"
 HEADER = "^(Hola, Soy TwargBot y existo para comentar con el texto del twitt linkeado) \n\n\n\n "
@@ -22,6 +24,19 @@ twitter = tweepy.API(auth)
 
 # Compile regex to match twitter urls
 TWITTER_REGEX_URL = re.compile("https://twitter\.com/.*")
+SHORTENER_URLS = re.compile("(https?:\/\/)(t\.co|bit.ly)(\/[a-zA-Z0-9]*)")
+
+
+def redir_status(stat):
+    return stat in [300, 301, 302, 303, 304, 305, 306, 307, 308]
+
+
+def unshorten_url(url):
+    return requests.head(url, allow_redirects=True).url
+
+
+def has_shortened_links(atweet):
+    return SHORTENER_URLS.search(atweet)
 
 
 def is_tweet(post):
@@ -44,16 +59,43 @@ def quote(text):
     return ">"+text
 
 
+def reddit_format_url(bare_url):
+    return f"[link]({bare_url})"
+
+
+def expand_tweet_urls(original_str):
+    """
+        Si el tweet tiene una shortened url, la reemplazo
+        por su full url. Si no, la devuelvo sin modificar
+    """
+    #matches = SHORTENER_URLS.findall(original_str)
+    for match in SHORTENER_URLS.finditer(original_str):
+        # for mathcobj in match -- falla porque siempre reemplaza la inmodificada
+        short_url = match.group(1)+match.group(2)+match.group(3)
+        expanded_url = unshorten_url(short_url)
+        formatted_url = reddit_format_url(expanded_url)
+        original_str = original_str.replace(short_url, formatted_url)
+
+    return original_str
+
+
+def parse_tweet(strtweet):
+    expanded_tw = expand_tweet_urls(strtweet)
+    # paso shurl a full url
+    expanded_tw = HEADER + quote(expanded_tw) + FOOTER
+    return expanded_tw
+
+
 def comment_post(apost):
     print("Preparandome para comentar..")
     status_id = extract_status_id(apost.url)
     status = twitter.get_status(status_id, tweet_mode="extended")
     tweet = status.full_text
-    print("El tweet es: \n" + tweet)
-    detailed_tweet = HEADER + quote(tweet) + FOOTER
-    apost.reply(detailed_tweet)
+    print("El tweet que lei del link es: \n" + tweet)
+    parsed_tweet = parse_tweet(tweet)
+    apost.reply(parsed_tweet)
     print("Comenté  con éxito.")
-    add_reply_to_db(apost)
+    add_to_replied(apost)
 
 
 def there_is_a_match(arg):
@@ -70,7 +112,7 @@ def on_visited_db(post):
     return there_is_a_match(c.fetchone())
 
 
-def add_reply_to_db(post):
+def add_to_replied(post):
     if not replied_db(post):
         # post_id text, author text, url text, title text
         c.execute('INSERT INTO replies5 VALUES (?,?,?,?)', (post.id, str(post.author), post.url, post.title))
@@ -83,26 +125,32 @@ def add_to_visited(post):
     conn.commit()
 
 
-def buscar_tweets():
+def buscar_tweets(cant):
     print("Buscando tweets...")
-    for i, post in enumerate(subreddit.new(limit=10)):
+    for i, post in enumerate(subreddit.new(limit=cant)):
+        time.sleep(1)
         if not on_visited_db(post):
-            add_to_visited(post)
             title = post.title
-            url = post.url
-            print(f"Analizando post {i}: {title}...")
+            print(f"Analizando post {i+1}: {title}...")
             if is_tweet(post) and not replied_db(post):
                 comment_post(post)
                 print("Title: ", post.title)
                 print("self.url: ", post.url)
                 print("----------------------")
-            print(f"Fin análisis post {i}")
+            add_to_visited(post)
+            print(f"Fin análisis post {i+1}")
+        else:
+            post.id
+            print(f"Ya visite el post {post.id}")
     conn.close()
     print("Finalizó mi busqueda")
 
 
 # MAIN
-buscar_tweets()
+buscar_tweets(50)
 
+# TODO; if it has a link expand it, then post it
 # TODO: execute every X minutes to fetch new posts for replying
+
+
 
